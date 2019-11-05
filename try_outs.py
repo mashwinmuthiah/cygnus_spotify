057197ca3be23ccf90a24323cbe9426465898cf5
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-11-04 20:30:23 -0500

    try_outs: Baseline

--- try_outs.py
+++ try_outs.py
@@ -2,6 +2,21 @@ import pandas as pd
 import spotipy
 import oAuth_spotify
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+import json
+from json.decoder import JSONDecodeError
+import spotipy.util as util
+
+client_credentials_manager = SpotifyClientCredentials(oAuth_spotify.Client_ID,oAuth_spotify.Client_Secret)
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+username = 'spotify'
+playlists = sp.user_playlists(username,limit = 3)
+
+
+print(json.dumps(playlists,indent=4))
+
+print('------END------')
 
 
 
